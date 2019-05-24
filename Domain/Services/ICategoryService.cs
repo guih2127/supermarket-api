aa6aaab4a7d2820d7d15234ec9fcff083de29678
Guilherme Reis Araujo <guih2127@gmail.com>
2019-05-24 02:01:52 -0300

    Metódo PUT finalizado

--- Domain/Services/ICategoryService.cs
+++ Domain/Services/ICategoryService.cs
@@ -9,5 +9,6 @@ namespace supermarket_api.Domain.Services
     {
          Task<IEnumerable<Category>> ListAsync();
          Task<SaveCategoryResponse> SaveAsync(Category category);
+         Task<SaveCategoryResponse> UpdateAsync(int id, Category category);
     }
 }
